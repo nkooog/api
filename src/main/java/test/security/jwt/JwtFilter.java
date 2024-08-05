da8ab0fa9204f35b882599d9793e158fc23d91a2
Jy_Park <nkooog@naver.com>
2024-08-05 09:15:48 +0900

    ing

--- src/main/java/test/security/jwt/JwtFilter.java
+++ src/main/java/test/security/jwt/JwtFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -13,13 +14,19 @@ import java.io.IOException;
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
+	private JwtTokenProvider provider;
+
+	public JwtFilter(JwtTokenProvider provider) {
+		this.provider = provider;
+	}
+
 	// http요청을 중간에 jwtFilter[커스터마이징 필터] 에서 토큰 검증
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-		String authorizationHeader = request.getHeader("Authorization");
+		String token = resolveToken(request);
 
-		if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+		if(token != null && provider.validateToken(token)) {
 			// 토큰 인증 검사
 
 
@@ -30,7 +37,13 @@ public class JwtFilter extends OncePerRequestFilter {
 			// error handler
 			throw new AuthenticationException("토큰이 없음"){};
 		}
+	}
 
-
+	private String resolveToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader("Authorization");
+		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
+		}
+		return null;
 	}
 }
--- src/main/java/test/security/jwt/JwtTokenProvider.java
+++ src/main/java/test/security/jwt/JwtTokenProvider.java
@@ -1,18 +1,23 @@
 package test.security.jwt;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 
@@ -20,7 +25,7 @@ import java.util.stream.Collectors;
 @Component
 public class JwtTokenProvider {
 
-	private final Key key;
+	private final SecretKey key;
 
 	public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
 		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
@@ -58,6 +63,51 @@ public class JwtTokenProvider {
 				.accessToken(accessToken)
 				.refreshToken(refreshToken)
 				.build();
+	}
+
+
+	//토큰으로부터 클레임을 만들고, 이를 통해 User 객체를 생성하여 Authentication 객체를 반환
+	public Authentication getAuthentication(String accessToken) {
+		Claims claims = parseClaims(accessToken);//accessToken에서 클레임을 가져온다.
+		if (claims.get("auth") == null) {
+			throw new RuntimeException("권한 정보가 담겨있지 않은 토큰입니다.");
+		}
+
+		Collection<? extends GrantedAuthority> authorities =
+				Arrays.stream(claims.get("auth").toString().split(","))
+						.map(SimpleGrantedAuthority::new)
+						.collect(Collectors.toList());//사용자의 Role과 권한을 조회하여 SimpleAuthority 목록을 authorities에 세팅한다.
+
+		UserDetails principal = new User(claims.getSubject(), "", authorities);//계정정보를 담은 User 객체를 생성한다.
+		return new UsernamePasswordAuthenticationToken(principal, "", authorities);//Authentication 객체를 반환한다.
+	}
+
+	private Claims parseClaims(String accessToken) {
+		try {
+			return Jwts.parser().verifyWith(this.key).build().parseClaimsJws(accessToken).getBody();
+		} catch (ExpiredJwtException e) {
+			return e.getClaims();
+		}
+	}
+
+	// 토큰 정보를 검증하는 메서드
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parser()
+					.verifyWith(this.key)
+					.build()
+					.parseClaimsJws(token);
+			return true;
+		} catch (SecurityException | MalformedJwtException e) {
+			log.info("Invalid JWT Token", e);
+		} catch (ExpiredJwtException e) {
+			log.info("Expired JWT Token", e);
+		} catch (UnsupportedJwtException e) {
+			log.info("Unsupported JWT Token", e);
+		} catch (IllegalArgumentException e) {
+			log.info("JWT claims string is empty.", e);
+		}
+		return false;
 	}
 
 	// LocalDateTime To Date
