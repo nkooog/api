f13c7aecd5175b5ef604c2a0555e83e4634f6742
Jy_Park <nkooog@naver.com>
2024-08-14 10:56:09 +0900

    swagger schema 추가

--- src/main/java/test/web/controller/user/MemberController.java
+++ src/main/java/test/web/controller/user/MemberController.java
@@ -1,9 +1,17 @@
 package test.web.controller.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,6 +32,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Slf4j
 @CrossOrigin(origins = "*", allowedHeaders = "*") // ??? 이거 뭐 어케 바꿔야함
+@Tag(name = "Member", description = "Member API")
 @RestController
 @RequestMapping(value = "/api/user", produces = MediaTypes.HAL_JSON_VALUE)
 public class MemberController {
@@ -64,6 +73,13 @@ public class MemberController {
 		return ResponseEntity.created(createURI).body(member);
 	}
 
+	@Operation(summary = "login", description = "사용자 로그인")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "성공",
+					content = {@Content(schema = @Schema(implementation = JwtToken.class))}),
+			@ApiResponse(responseCode = "404", description = "회원정보를 확인해주세요."),
+	})
+	@Parameter(name="loginId, password", description = "로그인 유저 ID, 패스워드", example = "member1", required = true)
 	@PostMapping("/login")
 	public ResponseEntity login(@RequestBody MemberDTO memberDTO, Errors errors) {
 
@@ -71,7 +87,7 @@ public class MemberController {
 		Authentication authentication = service.authenticate(memberDTO.getLoginId(), memberDTO.getPassword(), errors);
 
 		if(errors.hasErrors()) {
-			return ResponseEntity.badRequest().body(errors);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors);
 		}
 
 		token = provider.generateToken(authentication);
--- src/main/java/test/web/entity/user/MemberDTO.java
+++ src/main/java/test/web/entity/user/MemberDTO.java
@@ -1,5 +1,6 @@
 package test.web.entity.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
@@ -10,15 +11,19 @@ import lombok.*;
 @Setter
 public class MemberDTO {
 
+	@Schema(description = "회원 로그인 id")
 	@NotEmpty
 	private String loginId;
 
+	@Schema(hidden = true)
 //	@NotEmpty
 	private String name;
 
+	@Schema(description = "회원 패스워드")
 	@NotEmpty
 	private String password;
 
+	@Schema(hidden = true)
 //	@NotEmpty
 	private String email;
 
